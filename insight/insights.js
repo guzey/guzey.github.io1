9140310fd9551a6e0be55b431ad0af4ace1b0a30
Alex Guzey <alex@guzey.com>
2016-06-04 23:31:28 +0300

    add insight

--- insight/insights.js
+++ insight/insights.js
@@ -55,7 +55,8 @@ tweets = ["https://twitter.com/HiFromMichaelV/status/16952165247488001",
           "https://twitter.com/robinhanson/status/709883858355884034",
           "https://twitter.com/literalbanana/status/712913395645685760",
           "https://twitter.com/literalbanana/status/720188569675956224",
-          "https://twitter.com/Magic_of_expect/status/734970032283783169"
+          "https://twitter.com/Magic_of_expect/status/734970032283783169",
+          "https://mobile.twitter.com/vgr/status/738544615393918976"
          ];
 
 // 
