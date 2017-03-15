b7c8f4b8f7bfb3b85e3f5ea3c3100c7e19ef702b
Alex Guzey <alex@guzey.com>
2017-03-15 10:16:42 +0300

    variable levels and timeouts

--- x/js/main.js
+++ x/js/main.js
@@ -1,5 +1,3 @@
-// this was originally a breakout game, lol: http://breakout.enclavegames.com/
-// I will most likely remove any references to this in the future, as the code becomes rewritten entirely
 var canvas = document.getElementById("gameCanvas")
 var ctx = canvas.getContext("2d")
 var height = window.innerHeight
@@ -9,7 +7,7 @@ ctx.canvas.height = width*1.3
 var isRunning = true
 
 var wordPadding
-var lineSpeed = height*0.0003
+var lineSpeed = height*0.0001
 // var lineCount = 3
 var lineCount
 if (width < 1050) {
@@ -31,41 +29,35 @@ var words
 var wordsInRow
 
 // tfw when you realize js doesn't support modules yet
-var level0 = ['RAPIDLY'.split(' '), 1 ]
-var level1 = ['PRESS'.split(' '), 1 ]
-var level2 = ['X'.split(' '), 1 ]
-var level3 = ['TO'.split(' '), 1 ]
-var level4 = ['REFUSE'.split(' '), 1 ]
-var level5 = ['TO'.split(' '), 1 ]
-var level6 = ['COME'.split(' '), 1 ]
-var level7 = ['TO'.split(' '), 1 ]
-var level8 = ['TERMS'.split(' '), 1 ]
-var level9 = ['WITH'.split(' '), 1 ]
-var level10 = ['YOUR'.split(' '), 1 ]
-var level11 = ['OWN'.split(' '), 1 ]
-var level12 = ['MORTALITY'.split(' '), 1 ]
-// ...
-// 'Death'
-// 'is'
-// 'not'
-// 'real'
-var level13 = ['Death is not real.'.split(' '), 3]
+// [text, max_lines, max_time]
+var l0 = ['RAPIDLY'.split(' '), 1 ]
+var l1 = ['PRESS'.split(' '), 1 ]
+var l2 = ['X'.split(' '), 1 ]
+var l3 = ['TO'.split(' '), 1 ]
+var l4 = ['REFUSE'.split(' '), 1 ]
+var l5 = ['TO'.split(' '), 1 ]
+var l6 = ['COME'.split(' '), 1 ]
+var l7 = ['TO'.split(' '), 1 ]
+var l8 = ['TERMS'.split(' '), 1 ]
+var l9 = ['WITH'.split(' '), 1 ]
+var l10 = ['YOUR'.split(' '), 1 ]
+var l11 = ['OWN'.split(' '), 1 ]
+var l12 = ['MORTALITY'.split(' '), 1 ]
+var l16 = ['Death is not real.'.split(' '), 1]
+var l17 = ['Death is not real.'.split(' '), 1]
+var l18 = ['Death is not real.'.split(' '), 1]
+var l19 = ['Death is not real.'.split(' '), 2, 5]
+var l14 = ['Death is not real.'.split(' '), 1]
 
-// var gameLevels = [level0, level1, level2, level3]
-var gameLevels = [level0, level1, level2, level3, level4, level5, level6, level7, level8, level9,
-    level10, level11, level12, level13]
+// var gameLevels = [l12, l1, l2, l16, l17, l18, l19, l17]
+var gameLevels = [l0, l1, l2, l3, l4, l5, l6, l7, l8, l9, l10, l11, l12, l14, l19]
+// var gameLevels = [l19]
+// var gameLevels = [l0, l19]
+// var gameLevels = [l19, l7]
 var numLevels = gameLevels.length
 var currentLevelNum = 0
 var currentLevel = gameLevels[currentLevelNum]
-var endLevel = false
-
-
-// for(l=0; l<lineCount; l++) {
-//     words[l] = []
-//     for (w=0; w<wordsInRow; w++) {
-//         words[l][w] = { x: 0, y: 0, status: 1 }
-//     }
-// }
+var timeAtLevelStart
 
 function drawText(text, x, y, size, color) {
     ctx.font = size+"px Open Sans"
@@ -73,6 +65,7 @@ function drawText(text, x, y, size, color) {
     ctx.fillText(text, x, y)
 }
 
+// creates a matrix of level's words to align them later and check if they need to be shown
 function createLevelsWords() {
     words = []
     wordsInRow = currentLevel[0].length
@@ -93,14 +86,21 @@ function drawLevel() {
     // draw words!
     for (l=0; l<lineCount; l++) {
         wordPadding = 0
-        for (w=0; w<wordsInRow; w++) {
-            ctx.font = lineHeight+"px Open Sans"
+        for (w = 0; w < wordsInRow; w++) {
+            ctx.font = lineHeight + "px Open Sans"
             if (w > 0) {
-                wordPadding += ctx.measureText(currentLevel[0][w-1]+" ").width
+                wordPadding += ctx.measureText(currentLevel[0][w - 1] + " ").width
             }
-            if(words[l][w].status == 1) {
-                var wordX = wordPadding + lineOffsetLeft
-                var lineY = (currentRow * (lineHeight + linePadding)) + lineOffsetTop
+            if (words[l][w].status == 1) {
+                if (wordsInRow == 1) {
+                    ctx.textAlign = "center"
+                    var wordX = width/2
+                    var lineY = (currentRow * (lineHeight + linePadding)) + lineOffsetTop
+                } else {
+                    ctx.textAlign = "left"
+                    wordX = wordPadding + lineOffsetLeft
+                    lineY = (currentRow * (lineHeight + linePadding)) + lineOffsetTop
+                }
                 // this allows us to check whether the first existing word is touching the upper bound, thus
                 // triggering some event
                 if (firstWord == false) {
@@ -111,7 +111,7 @@ function drawLevel() {
                 drawText(currentLevel[0][w], wordX, lineY, lineHeight, "000000")
             } else if (l == lastWordL && w == lastWordW) {
             }
-            if (currentWordsInRow < wordsInRow-1) {
+            if (currentWordsInRow < wordsInRow - 1) {
                 currentWordsInRow++
             } else {
                 currentRow += 1
@@ -119,22 +119,35 @@ function drawLevel() {
             }
         }
     }
-    // if (firstWord[0] == lineCount-1 && firstWord[1] == wordsInRow-1) {
-    //     drawText('You Won!', 150, 150, 48, "dd2bda")
-    //     endLevel = true
-    // }
     if (words[lineCount-1][wordsInRow-1].status == 0) {
         drawText('You Won!', 150, 150, 48, "dd2bda")
-        currentLevelNum += 1
-        if (currentLevelNum < numLevels) {
-            currentLevel = gameLevels[currentLevelNum]
-            runLevel(currentLevel)
+        startNextLevel()
+        }
+    // if the level consists of several lines
+    else if (currentLevel[1] > 1 && isRunning == true) {
+        if (Date.now()-timeAtLevelStart < currentLevel[2]*1000) {
+            lineOffsetTop -= lineSpeed
+            drawText(Date.now(), 50, 250, 48, "dd2bda")
+        } else {
+            startNextLevel()
         }
     }
 
     requestAnimationFrame(function() {
         drawLevel()
     })
+}
+
+function startNextLevel() {
+    currentLevelNum += 1
+    if (currentLevelNum < numLevels) {
+        currentLevel = gameLevels[currentLevelNum]
+        runLevel(currentLevel)
+    } else {
+        ctx.clearRect(0, 0, canvas.width, canvas.height)
+        drawText('That\'s it!', 50, 150, 32, "0d2bda")
+        // break
+    }
 }
 
 function runLevel() {
@@ -161,7 +174,7 @@ function runLevel() {
         linePadding = lineHeight * 0.5
         lineOffsetTop = lineHeight * 1.5
     }
-
+    timeAtLevelStart = Date.now()
     drawLevel()
 }
 
@@ -217,10 +230,10 @@ function openMenu() {
 }
 function clickMenu() {
     if (isRunning == true) {
-        // cancelAnimationFrame(myReq)
+        timeAtPause = Date.now()
         openMenu()
     } else {
-        // requestAnimationFrame(draw)
+        timeAtLevelStart += (Date.now() - timeAtPause)
     }
     isRunning = !isRunning
 
